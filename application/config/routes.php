90375bc8ecd0999c419e4e14c9802a3987c13541
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-06-29 11:11:10 -0300

    added form validation to edit and add users, inserted method logout

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,6 +5,7 @@ $route['default_controller'] = '';
 
 /* Routes Users*/
 $route['auth'] = 'user/user';
+$route['logout'] = 'user/user/logout';
 $route['addedit'] = 'user/user/add_edit';
 $route['details/(:num)'] = 'user/user/details/$1';
 
--- application/modules/user/controllers/user.php
+++ application/modules/user/controllers/user.php
@@ -7,7 +7,7 @@ class User extends MX_Controller {
 	{
 		parent::__construct();
 		$this->load->model("user_model");
-		$this->load->helper('email');
+		$this->load->library('form_validation');
 	}
 
 	public function index()
@@ -28,22 +28,36 @@ class User extends MX_Controller {
 						->set_content_type('application/json')
 						->set_output(json_encode(array('error' => 'Usuário ou senha incorretos!')));
 		}
+	}	
+
+	public function logout(){
+		$this->session->sess_destroy();
+		return $this->output
+						->set_content_type('application/json')
+						->set_output(json_encode(array('success' => 'Logout realizado com sucesso!')));
 	}
 
 	public function add_edit()
 	{
-		$id = trim($this->input->post('id'));
-		$name = trim($this->input->post('name'));
-		$email = trim($this->input->post('email'));
-		$password = $this->input->post('password');
-		$nivel = trim($this->input->post('nivel'));
+		$this->form_validation->set_rules('name', 'Nome', 'trim|required|min_length[5]|max_length[30]');
+		$this->form_validation->set_rules('password', 'Senha', 'trim|required|min_length[6]');
+		$this->form_validation->set_rules('passconf', 'Confirmação de senha', 'trim|required|matches[password]');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[users.email]');
+		$this->form_validation->set_rules('nivel', 'Nivel', 'trim|required|max_length[1]');
 
-		if(empty($name) || empty($email) ||!valid_email($email) || empty($password) || empty($nivel)) {
+		if($this->form_validation->run() == FALSE) {
 			return $this->output
 							->set_status_header(409)
 							->set_content_type('application/json')
-							->set_output(json_encode(array('error' => 'Preencha os dados corretamente!')));
+							->set_output(json_encode(array('error' => validation_errors())));
 		}else{
+			
+			$id = trim($this->input->post('id'));
+			$name = trim($this->input->post('name'));
+			$email = trim($this->input->post('email'));
+			$password = $this->input->post('password');
+			$nivel = trim($this->input->post('nivel'));
+
 			if($id) {
 				$data = array_filter(array(
 					'id' => $id,
@@ -63,16 +77,7 @@ class User extends MX_Controller {
 								->set_content_type('application/json')
 								->set_output(json_encode(array('error' => 'Erro ao atualizar!')));
 				}
-			}else{				
-				$user = $this->user_model->check_email($email);
-
-				if($user) {			
-					return $this->output
-								->set_status_header(409)
-								->set_content_type('application/json')
-								->set_output(json_encode(array('error' => 'Usuário ja cadastrado!')));
-				}
-
+			}else{
 				if($this->user_model->add($name, $email, sha1(md5(base64_encode($password))), $nivel)) {
 					return $this->output
 								->set_content_type('application/json')
