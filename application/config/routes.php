a70cc1a1e7fd02df6fe52ffeaa467e472bcd1a53
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-07-02 14:49:26 -0300

    added route for password recovery

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,4 +1,5 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = '';
@@ -8,6 +9,7 @@ $route['auth'] = 'user/user';
 $route['logout'] = 'user/user/logout';
 $route['addedit'] = 'user/user/add_edit';
 $route['details/(:num)'] = 'user/user/details/$1';
+$route['recover'] = 'user/user/recover';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
