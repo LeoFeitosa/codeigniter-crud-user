1c8f42d18019e16d3b9a62f9afbd34f8ab9e143b
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-07-03 14:36:00 -0300

    helper to verify that the user is logged in

--- application/helpers/user_helper.php
+++ application/helpers/user_helper.php
@@ -1,11 +1,22 @@
-
 <?php
 if(!defined('BASEPATH')) exit('No direct script access allowed');
  
 if(!function_exists('check_login')) {
 	function is_logged(){
 		$CI = & get_instance();
-		return ((empty($CI->session->userdata('user'))) ? false : true);
+
+		$user_id = (!empty($CI->session->userdata('user')[0]->id) ? $CI->session->userdata('user')[0]->id : null);
+
+		$CI->load->database();
+		
+		$session = $CI->db->select('session_id')
+								->where('id', $user_id)
+								->get('users')
+								->row('session_id');
+
+		$query = $CI->db->get_where('ci_sessions', array('id' => $session))->result();
+
+		return ((empty($query)) ? false : true);
 	}
 }
 
